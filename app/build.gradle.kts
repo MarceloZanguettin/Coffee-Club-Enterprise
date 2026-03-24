7b65273f10d814a16f97a995fde3e6307d59e483
Marcelo Zanguettin Pereira <mzanguettin@gmail.com>
2026-03-24 09:07:33 -0300

    Implementado as bibliotecas e dependencias necessárias para o projeto

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,14 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.coffeeclubenterprise"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.coffeeclubenterprise"
         minSdk = 26
-        targetSdk = 36
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -46,6 +44,11 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+
+    implementation(libs.androidx.material.icons.extended)
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.navigation.runtime.ktx)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
